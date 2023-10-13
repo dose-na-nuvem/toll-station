e5b0dd31fa2257387721495352363e80616f5f8a
Luiz Guilherme Ribeiro <ltrindaderibeiro@gmail.com>
2023-10-13 09:47:37 -0300

    Luizgribeiro/#5 (#20)
    * chore: add go package files
    
    Signed-off-by: Luiz Ribeiro <ltrindaderibeiro@gmail.com>
    
    * feat: add bare http server
    
    Signed-off-by: Luiz Ribeiro <ltrindaderibeiro@gmail.com>
    
    ---------
    
    Signed-off-by: Luiz Ribeiro <ltrindaderibeiro@gmail.com>

---
+++ pkg/server/http.go
@@ -0,0 +1,85 @@
+package server
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/dose-na-nuvem/toll-station/config"
+	"go.uber.org/zap"
+)
+
+var errNoTLSConfig = errors.New("servidor sem configuração de TLS")
+
+type HTTP struct {
+	logger *zap.Logger
+
+	srv *http.Server
+
+	certFile    string
+	certKeyFile string
+}
+
+func NewHTTP(cfg *config.Cfg, /*tollStationHandler http.Handler*/) (*HTTP, error) {
+	mux := http.NewServeMux()
+	//mux.Handle("/", )
+
+	srv := &http.Server{
+		Addr:              cfg.Server.HTTP.Endpoint,
+		Handler:           mux,
+		ReadHeaderTimeout: cfg.Server.HTTP.ReadHeaderTimeout * time.Millisecond,
+	}
+
+	return HTTPWithServer(cfg, srv)
+}
+
+func HTTPWithServer(cfg *config.Cfg, srv *http.Server) (*HTTP, error) {
+	// if cfg.Server.TLS.CertFile != "" && cfg.Server.TLS.CertKeyFile != "" {
+	// 	cfg.Logger.Info("Servidor HTTP configurado com opções TLS",
+	// 		zap.String("cert_file", cfg.Server.TLS.CertFile),
+	// 		zap.String("cert_key_file", cfg.Server.TLS.CertKeyFile),
+	// 	)
+	// } else {
+	// 	if cfg.Server.TLS.Insecure {
+	// 		cfg.Logger.Info("Servidor HTTP sem configurações de TLS! Este servidor está inseguro!")
+	// 	} else {
+	// 		return nil, errNoTLSConfig
+	// 	}
+	// }
+
+	return &HTTP{
+		logger:      cfg.Logger,
+		srv:         srv,
+		// certFile:    cfg.Server.TLS.CertFile,
+		// certKeyFile: cfg.Server.TLS.CertKeyFile,
+	}, nil
+}
+
+func (h *HTTP) Start(_ context.Context, errorChan chan error) {
+	var err error
+
+	go func() {
+		if h.certFile != "" && h.certKeyFile != "" {
+			err = h.srv.ListenAndServeTLS(h.certFile, h.certKeyFile)
+		} else {
+			err = h.srv.ListenAndServe()
+		}
+
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			h.logger.Error("falha ao iniciar o servidor HTTP", zap.Error(err))
+			errorChan <- fmt.Errorf("falha ao iniciar o servidor HTTP %w", err)
+		}
+	}()
+}
+
+func (h *HTTP) Shutdown(ctx context.Context) error {
+	// We received an interrupt signal, shut down.
+	if err := h.srv.Shutdown(ctx); err != nil {
+		// Error from closing listeners, or context timeout:
+		return err
+	}
+
+	return nil
+}
